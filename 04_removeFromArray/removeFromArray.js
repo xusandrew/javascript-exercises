0028ff78d790334244e45ec3096e0c174d2c3872
Andrew Xu <xusandrew@gmail.com>
2022-12-03 20:54:58 -0500

    Complete 04-removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++){
+        let index = array.indexOf(args[i]);
+        if (index > -1){
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
