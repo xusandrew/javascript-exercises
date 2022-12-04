c709e0bb9f3a6a4cfa07bbc5453ac7591f9f10ac
Andrew Xu <xusandrew@gmail.com>
2022-12-03 20:42:30 -0500

    Complete 03-reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const chars = string.split("");
+    let ans = '';
+    for (let i = 0; i < chars.length; i++){
+        ans += chars[chars.length - 1 - i];
+    }
+    return ans;
 };
 
 // Do not edit below this line
