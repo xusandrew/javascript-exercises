6e7af380ada451de885392d46fde1530529acd7f
Andrew Xu <xusandrew@gmail.com>
2022-12-03 21:25:00 -0500

    Complete 07-tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (f) {
+  let temp = (f - 32) * 5/9;
+  temp = Math.round(temp * 10)/10;
+  return temp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (c) {
+  let temp = c * (9/5) + 32;
+  temp = Math.round(temp * 10)/10;
+  return temp;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
