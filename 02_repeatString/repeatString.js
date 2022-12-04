7abf28c0724a2db7841ee4d2ca52be348685b014
Andrew Xu <xusandrew@gmail.com>
2022-12-03 20:38:20 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let ans = '';
+    for (let i = 0; i < num; i++){
+        ans += string;
+    }
+    return ans;
 };
 
 // Do not edit below this line
