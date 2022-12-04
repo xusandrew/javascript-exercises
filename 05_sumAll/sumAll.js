9b700d36ba9699bfd6537c148ed3ca05f4a39d9e
Andrew Xu <xusandrew@gmail.com>
2022-12-03 21:03:16 -0500

    Complete 05-sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0)
+        return 'ERROR';
+    if (!Number.isInteger(start) || !Number.isInteger(end))
+        return 'ERROR';
 
+    if (end < start)
+        end = [start, start = end][0];
+
+    let sum = 0;
+    for (let i = start; i <= end; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
