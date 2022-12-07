cf76eb2c832a45f672b61fd2bb3e5125b1377f6a
Andrew Xu <xusandrew@gmail.com>
2022-12-07 16:14:27 -0500

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        const oldAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldAge > currAge ? oldest : current;
+    });
+};  
 
-};
+const getAge = function(birth, death){
+    if (!death) death = new Date().getFullYear();
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
